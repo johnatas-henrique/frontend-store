d621d79561687a7eb57ec5e0720855955a009fc5
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-02-24 20:02:12 -0300

    Carrinho - Início - Precisamos de quantidade no localStorage

--- src/components/ItemCarrinho.jsx
+++ src/components/ItemCarrinho.jsx
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import CarrinhoVazio from './CarrinhoVazio';
 
 class ItemCarrinho extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: this.props.itemsId,
-      teste: ['laranjas', 'bananas', 'abacates'],
-    };
-  }
 
-  render () {
-    const { items, teste } = this.state;
-    if (items === '1') return <CarrinhoVazio />;
+  render() {
+    const { itensCarrinho, carrinhoVazio } = this.props;
+    if (carrinhoVazio) return <CarrinhoVazio />;
     return (
-      { teste.map(((item, index) => <p key={item}>{index} - {item} do teste</p>)) }
-    )
+      <div>
+        {itensCarrinho.map((list) => (
+          <div className="itemBusca" key={list.id}>
+            {list.title}
+            <p>
+              R$
+              {list.price}
+            </p>
+            <img className="itemImage" src={list.thumbnail} alt={list.title} />
+          </div>
+        ))}
+      </div>
+    );
   }
 }
+
+ItemCarrinho.propTypes = {
+  carrinhoVazio: PropTypes.bool.isRequired,
+  itensCarrinho: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    price: PropTypes.number,
+    thumbnail: PropTypes.string,
+    title: PropTypes.string,
+    attributes: PropTypes.array,
+  })).isRequired,
+};
+
 
 export default ItemCarrinho;
--- src/pages/Carrinho.js
+++ src/pages/Carrinho.js
@@ -3,12 +3,37 @@ import { Link } from 'react-router-dom';
 import ItemCarrinho from '../components/ItemCarrinho';
 
 class Carrinho extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      carrinhoVazio: true,
+      itensCarrinho: [],
+    };
+  }
+
+  componentDidMount() {
+    this.funcaoProCCMount();
+  }
+
+  funcaoProCCMount() {
+    const guardar = JSON.parse(localStorage.getItem('Produtos') || '[]');
+    if (guardar.length > 0) {
+      this.setState({
+        itensCarrinho: guardar,
+        carrinhoVazio: false,
+      });
+    }
+  }
+
   render() {
+    const { itensCarrinho, carrinhoVazio } = this.state;
     return (
       <div className="Carrinho">
         <h1>Carrinho de Compras</h1>
-        <ItemCarrinho itemsId='1' />
-        <br />
+        <ItemCarrinho 
+          itensCarrinho={itensCarrinho}
+          carrinhoVazio={carrinhoVazio}
+        />
         <Link to="/">Voltar</Link>
       </div>
     );
