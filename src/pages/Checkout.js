f49e212591f0afd2a81c8c155b34b4634591ae5f
lauro <laurolyra@gmail.com>
2020-02-26 19:41:32 -0300

    alterações que não me lembro onde fiz

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,15 +5,38 @@ class Checkout extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      carrinhoVazio: true,
-      itensCarrinho: [],
+      listaVazia: false,
     };
+    this.setaListaVazia = this.setaListaVazia.bind(this);
+  }
+
+  componentDidMount() {
+    const prodSalvos = JSON.parse(localStorage.getItem('Produtos'));
+    if (prodSalvos.length === 0) {
+      this.setaListaVazia(true);
+    }
+  }
+
+  setaListaVazia(parametro) {
+    this.setState({
+      listaVazia: parametro,
+    });
   }
 
   render() {
+    const { listaVazia } = this.state;
+    if (listaVazia) {
+      return (
+        <div className="Carrinho">
+          <h1>Pagina Checkout</h1>
+          <h2>O Carrinho está vazio</h2>
+          <Link to="/">Continuar comprando</Link>
+        </div>
+      );
+    }
     return (
-      <div className="Carrinho">
-        <h1>Pagina Checkout</h1>
+      <div>
+        <h1>Confira os produtos!</h1>
         <Link to="/">Continuar comprando</Link>
       </div>
     );
