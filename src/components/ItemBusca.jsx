0d18caffb286b6e351f714e0dce9bda1b0f15da3
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-02-26 18:04:45 -0300

    card 04 - alteração nos fetchs

--- src/components/ItemBusca.jsx
+++ src/components/ItemBusca.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import './ItemBusca.css';
 
 class ItemBusca extends Component {
   static linkETitle(list) {
