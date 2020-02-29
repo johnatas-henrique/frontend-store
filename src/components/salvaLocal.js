06264b8ea2fa7d3915f928aba76f7706785707ea
lauro <laurolyra@gmail.com>
2020-02-29 11:02:05 -0300

    adição de itens no carrinho organizada.

--- src/components/salvaLocal.js
+++ src/components/salvaLocal.js
@@ -1,7 +1,7 @@
 export default function salvaLocal(obj1, obj2, quantState, id) {
   const firstObject = obj1;
   let secondObject = obj2;
-  firstObject.quant = quantState;
+  firstObject.quant += quantState;
   secondObject = secondObject.filter((item) => item.id !== id);
   secondObject.push(firstObject);
   localStorage.setItem('Produtos', JSON.stringify(secondObject));
