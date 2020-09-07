d052227594101956fc6daf6659baef35e9945c7b
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-09-07 18:00:22 -0300

    Updates

---
+++ precache-manifest.f2591080a25ab1cec04dd984628aff94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c10816a6702b54933cd08604aa974fc",
+    "url": "/frontend-store/index.html"
+  },
+  {
+    "revision": "9f032658ded9d0767ce7",
+    "url": "/frontend-store/static/css/main.47972693.chunk.css"
+  },
+  {
+    "revision": "efb1a6af8f17768beadf",
+    "url": "/frontend-store/static/js/2.bbae2dcc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/frontend-store/static/js/2.bbae2dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f032658ded9d0767ce7",
+    "url": "/frontend-store/static/js/main.09604315.chunk.js"
+  },
+  {
+    "revision": "ac5cf20b832f1151793b",
+    "url": "/frontend-store/static/js/runtime-main.0aa20018.js"
+  },
+  {
+    "revision": "50cacb035cb5c13130abf62336614057",
+    "url": "/frontend-store/static/media/background.50cacb03.jpg"
+  },
+  {
+    "revision": "ee36df287adec84f2aa1caad0fbcb0fc",
+    "url": "/frontend-store/static/media/caixa-vazia.ee36df28.png"
+  },
+  {
+    "revision": "361c932e0efb4a13ea7b2e68dce98ae4",
+    "url": "/frontend-store/static/media/lupa.361c932e.svg"
+  }
+]);
